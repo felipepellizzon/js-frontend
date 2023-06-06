5648cdfca9bb1a887f56b5bf0eff310c00189b57
felipepellizzon <fapellizzon@gmail.com>
2023-06-05 23:38:56 -0300

    add: finish project

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ function changeText() {
     return (span.innerText = oldText);
   }
 }
+
+function applyClass() {
+  let main_div = document.getElementById("main_div");
+  let new_title = document.getElementById("title");
+  let new_sub_title = document.getElementById("sub-title");
+  let new_span = document.querySelectorAll("span");
+
+  main_div.classList.add("new-main");
+  new_title.classList.add("new-title");
+  new_sub_title.classList.add("new-sub-title");
+  new_span.forEach((el) => {
+    console.log("aoba");
+    el.classList.add("new-span");
+  });
+}
+
+function appearCard() {
+  let element = document.getElementById("card");
+  let span = document.getElementById("span_card");
+
+  span.style.visibility = "hidden";
+
+  element.style.visibility = "visible";
+}
